0a69e28af1f61270df1417d641146b87b8b70dc5
Mariia <mytsko.mariia@gmail.com>
2020-09-27 12:42:12 +0200

    Updates

---
+++ precache-manifest.5a90c44f88f10d25e4951273134c6f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14eee08107a83dc52573615e1f8538e",
+    "url": "/HW18/index.html"
+  },
+  {
+    "revision": "b66ce3ccba5c937c1abb",
+    "url": "/HW18/static/css/main.fedfe501.chunk.css"
+  },
+  {
+    "revision": "ee74143330ee43db9b16",
+    "url": "/HW18/static/js/2.ea096656.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW18/static/js/2.ea096656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66ce3ccba5c937c1abb",
+    "url": "/HW18/static/js/main.761464bf.chunk.js"
+  },
+  {
+    "revision": "14d233e22f12fdd0c3f0",
+    "url": "/HW18/static/js/runtime-main.43be57a1.js"
+  }
+]);
